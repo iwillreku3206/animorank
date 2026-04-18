457a6def1f55918ac95fbb1539014418ccbee914
Rinaldo Lee <37181282+iwillreku3206@users.noreply.github.com>
2026-04-19 01:30:19 +0800

    feat: added loggging module and framework for DI

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,15 @@
-export { handle } from '$lib/auth'
+import type { Handle } from '@sveltejs/kit';
+import { handle as authHandle } from '$lib/auth';
+import { ServiceProvider } from '$lib/services/serviceProvider';
+import { Logger } from '$lib/logging/logger';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const serviceProvider = ServiceProvider.instance()
+  const logger = serviceProvider.getService(Logger)
+
+  const response = await authHandle({ event, resolve });
+
+  logger.debug(`${event.request.method} ${event.url.pathname}`)
+
+  return response;
+};
---
+++ src/lib/logging/file.ts
@@ -0,0 +1,26 @@
+import { Logger, LogLevelNames, type Loggable } from "./logger";
+import fs from 'fs/promises'
+
+export class FileLogger extends Logger {
+  timeFormat: Intl.DateTimeFormat
+
+  constructor(module: string, timeFormat?: Intl.DateTimeFormat) {
+    super(module)
+
+    this.timeFormat = timeFormat || new Intl.DateTimeFormat('en', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
+  }
+
+  protected async log(message: Loggable): Promise<void> {
+    const dateTime = new Date();
+    const formatted = this.timeFormat.format(dateTime)
+
+    await fs.appendFile(process.env.FILE_LOGGER_FILE || '/tmp/animorank.log', Buffer.from(`[${LogLevelNames[message.level]}] [${formatted}] [${this.module}] ${message.message}\n`), 'utf8')
+  }
+}
