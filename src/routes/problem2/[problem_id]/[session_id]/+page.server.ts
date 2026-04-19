70cf96de74a68b54a9d742e138c269edfbcc21c3
Rinaldo Lee <37181282+iwillreku3206@users.noreply.github.com>
2026-04-19 19:07:29 +0800

    fix: student sessions were not getting checked

--- src/routes/problem2/[problem_id]/[session_id]/+page.server.ts
+++ src/routes/problem2/[problem_id]/[session_id]/+page.server.ts
@@ -3,26 +3,26 @@ import type { PageServerLoad } from './$types';
 import { db } from '$lib/zenstack';
 
 export const load: PageServerLoad = async ({ params, locals }) => {
-	const session = await locals.auth();
+  const session = await locals.auth();
 
-	if (!session || !session.user.id) redirect(302, '/about');
+  if (!session || !session.user.id) redirect(302, '/about');
 
-	const problem = await db.problem.findUnique({
-		where: {
-			id: params.problem_id,
-			problem_set: {
-				OR: [{ subscriptions: { some: { student_id: session.user.id } } }, { is_global: true }]
-			}
-		},
-		include: { practice_sessions: { where: { student_id: session.user.id } } }
-	});
-	if (!problem) throw error(404, { message: 'Not Found' });
+  const problem = await db.problem.findUnique({
+    where: {
+      id: params.problem_id,
+      problem_set: {
+        OR: [{ subscriptions: { some: { student_id: session.user.id } } }, { is_global: true }]
+      }
+    },
+    include: { practice_sessions: { where: { student_id: session.user.id } } }
+  });
+  if (!problem) throw error(404, { message: 'Not Found' });
 
-	const practiceSession = await db.practiceSession.findUnique({
-		where: { id: params.session_id }
-	});
+  const practiceSession = await db.practiceSession.findUnique({
+    where: { id: params.session_id, student_id: session.user.id }
+  });
 
-	if (!practiceSession) throw redirect(302, `/problem2/${params.problem_id}`);
+  if (!practiceSession) throw redirect(302, `/problem2/${params.problem_id}`);
 
-	return { problem, practiceSession };
+  return { problem, practiceSession };
 };
