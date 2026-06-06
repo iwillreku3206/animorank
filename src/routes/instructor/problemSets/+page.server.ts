00e4b1b75d18bc8947ed95808da0672d11925d23
Rinaldo C. Lee <37181282+iwillreku3206@users.noreply.github.com>
2026-06-06 17:18:38 +0800

    working: problem set instructor page

--- src/routes/instructor/problemSets/+page.server.ts
+++ src/routes/instructor/problemSets/+page.server.ts
@@ -1,5 +1,38 @@
+import { ProblemSetService } from '$lib/problemSet';
+import { ServerServiceProvider } from '$lib/services/serverServiceProvider';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import type { SortOrder, SortType } from '$lib/problemSet/problemSetService';
 
-export const load: PageServerLoad = () => {
-  return;
+export const load: PageServerLoad = async ({ locals, url }) => {
+  const session = await locals.auth();
+  if (!session || !session.user.id) return redirect(302, '/');
+
+  const serviceProvider = ServerServiceProvider.instance();
+  const problemSetService = serviceProvider.getService(ProblemSetService);
+
+  let sortBy = url.searchParams.get('sortBy') || '';
+  if (!['', 'problems_solved', 'problem_count', 'completion_pct', 'difficulty'].includes(sortBy))
+    sortBy = '';
+  const sortOrder = url.searchParams.get('sortOrder') === 'desc' ? 'desc' : 'asc';
+
+  const { problemSets, tags, total } = await problemSetService.findByFilter({
+    user: session.user,
+    pageSize: 12,
+    page: Number(url.searchParams.get('page') || 1),
+    filters: {
+      search: url.searchParams.get('search') || '',
+      tags: url.searchParams.getAll('tag')
+    },
+    sort: {
+      by: sortBy as SortType,
+      order: sortOrder as SortOrder
+    }
+  });
+
+  return {
+    tags,
+    pageCount: Math.ceil(total / 12),
+    problemSets
+  };
 };
