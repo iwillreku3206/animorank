a0ee7ba7df4883b7c5b22efd1f0953a4a753e3c7
Rinaldo C. Lee <37181282+iwillreku3206@users.noreply.github.com>
2026-06-23 19:26:10 +0800

    feat: made pts logo clickable

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -18,11 +18,18 @@
           <span class="font-display text-3xl font-bold">AnimoRank</span>
         </div>
         <p class="text-sm text-base-content/70">Built by Lasallians, for Lasallians.</p>
-        <img
-          src="/img/pts-logo.png"
-          alt="PTS logo"
-          class="object-contain object-left h-16"
-        />
+
+        <!-- PTS -->
+        <a
+          href="https://dlsupts.vercel.app"
+          target="_blank"
+        >
+          <img
+            src="/img/pts-logo.png"
+            alt="PTS logo"
+            class="object-contain object-left h-16"
+          />
+        </a>
       </div>
 
       <!-- Support links -->
